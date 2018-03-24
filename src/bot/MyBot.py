c7745c49c30d17e74f5412fa50927ddc2f38efd5
peakyDicers <ian.chui@hotmail.com>
2018-03-24 09:01:50 -0400

    distance works

--- src/bot/MyBot.py
+++ src/bot/MyBot.py
@@ -1,4 +1,5 @@
 from src.bot.Bot import Bot
+import copy
 
 
 class MyBot(Bot):
@@ -13,4 +14,30 @@ class MyBot(Bot):
     def turn(self, game_state, character_state, other_bots):
         # Your bot logic goes here
         super().turn(game_state, character_state, other_bots)
+
+        copy_game_state = [list(state) for state in copy.deepcopy(game_state).split('\n')]
+        print(len(copy_game_state))
+
+        
+        #get old bot location. 
+        old_evilbot_locations = []
+        for bot in other_bots:
+          print(bot)
+          old_evilbot_locations.append(bot['location'])
+        print(old_evilbot_locations)
+
+        #check new bot location and current bot location. 
+        def checkNearbyBot():
+          for i in range other_bots:
+            deltaX = other_bots[i]['location'][0] - self['location'][0]
+            print deltaX
+
+            #if other_bots[i]['location']
+            
+
+        
         return self.commands.idle()
+        
+
+        
+
